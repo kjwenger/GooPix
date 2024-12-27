4eb1e23c7f2395f4acec2601f8b4bf61436e39a9
Klaus Wenger <kjwenger@yahoo.com>
2024-12-27 22:31:19 +0100

    created xcode project in category subfolder

---
+++ xcode/GooPix/GooPix/GooPixApp.swift
@@ -0,0 +1,17 @@
+//
+//  GooPixApp.swift
+//  GooPix
+//
+//  Created by Klaus Wenger on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GooPixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xcode/GooPix/GooPixTests/GooPixTests.swift
@@ -0,0 +1,16 @@
+//
+//  GooPixTests.swift
+//  GooPixTests
+//
+//  Created by Klaus Wenger on 12/27/24.
+//
+
+import Testing
+
+struct GooPixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
